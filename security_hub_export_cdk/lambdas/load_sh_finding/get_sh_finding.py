eec06d35d6f82ef64a63830affd1906fcc858ebc
Jonathan Nguyen <tamg@amazon.com>
2022-07-13 16:53:08 -0500

    updated lambda code for filter

--- security_hub_export_cdk/lambdas/load_sh_finding/get_sh_finding.py
+++ security_hub_export_cdk/lambdas/load_sh_finding/get_sh_finding.py
@@ -22,55 +22,30 @@ sechub = boto3.client('securityhub')
 s3 = boto3.resource('s3')
 ssm = boto3.client('ssm')
 
-def create_filter ():
-    day_counter = 365
-    now = datetime.datetime.now()
-    start_date_to_get_findings = now
-    end_date_to_get_findings = now - datetime.timedelta(days=day_counter)
-    start_date_to_get_findings = start_date_to_get_findings.replace(
-        hour=0, minute=0, second=0, microsecond=0)
-    end_date_to_get_findings = end_date_to_get_findings.replace(
-        hour=23, minute=59, second=59, microsecond=59)
-    
-    logger.info("Creating finding filter to get findings from {} to {}...".format(start_date_to_get_findings,end_date_to_get_findings))
+def create_filter (date_filter):
+    converted_string_date = datetime.datetime.strptime(date_filter, '%Y-%m-%dT%H:%M:%S.%fZ')
+    day_counter = 90
+    updatedat_end = str(converted_string_date.isoformat()[:-3]+'Z')
+    updatedat_start = str((converted_string_date - datetime.timedelta(days=day_counter)).isoformat()[:-3]+'Z')
+    logger.info("Creating finding filter to get findings from {} to {}...".format(updatedat_start,updatedat_end))
 
     finding_filter = {
-                #'UpdatedAt': [
-                #    {
-                #        'Start': start_date_to_get_findings.strftime('%Y-%m-%dT%H:%M'),
-                #        'End': end_date_to_get_findings.strftime('%Y-%m-%dT%H:%M')
-                        # 'DateRange': {
-                        #     'Value': 1,
-                        #     'Unit': 'DAYS'
-                        # }
-                #    },
-                #],
-                # "Region": [
-                #     {
-                #         "Value": REGION,
-                #         "Comparison": "EQUALS"
-                #     }
-                # ],
-                #"WorkflowStatus": [
-                #    {
-                #        "Value": "NEW",
-                #        "Comparison": "EQUALS"
-                #    },
-                #    {
-                #        "Value": "NOTIFIED",
-                #        "Comparison": "EQUALS"
-                #    }
-                #],
-                # "RecordState": [
-                #     {
-                #         "Value": "ACTIVE",
-                #         "Comparison": "EQUALS"
-                #     },
-                # ]
+                'UpdatedAt': [
+                    {
+                        'Start': updatedat_start,
+                        'End': updatedat_end
+                    },
+                ],
+                 "RecordState": [
+                     {
+                         "Value": "ACTIVE",
+                         "Comparison": "EQUALS"
+                     },
+                 ]
             }
     return finding_filter
 
-def get_findings (finding_filter, next_token):
+def get_findings (sechub, finding_filter, next_token):
     max_iterator = 50
     results=[]
     logger.info("Running export for Security Hub findings...")
@@ -119,39 +94,37 @@ def sechub_count_value (results):
     return sechub_count
 
 def put_obj_to_s3(results, consolidated_results):
-    key = datetime.datetime.now().strftime('%Y/%m/%d') + "/security-hub-finding-export" + str(uuid.uuid4()) + ".json"
-    logger.info("Exporting {} findings to s3://{}/{}".format(len(results), S3_BUCKET, key))
-    max_iterator = 3
-    for x in range(0, max_iterator, 1):
-        while True:
-            try:
-                s3.Bucket(S3_BUCKET).put_object(
-                Key=key,
-                Body=consolidated_results
-                )
-                logger.info("Successfully exported {} findings to s3://{}/{}".format(len(results), S3_BUCKET, key))
-            except ClientError as error_handle:
-                    if error_handle.response['Error']['Code'] == 'ConnectTimeoutError':
-                        time.sleep(5)
-                        logger.warning('Catching Connection Timeout Error...')
-                        continue
-            except Exception as exception_handle:
-                    logger.error(exception_handle)  
-            break
+    key = datetime.datetime.now().strftime('%Y/%m/%d/%H') + "/security-hub-finding-export" + str(uuid.uuid4()) + ".json"
+    try:
+        logger.info("Exporting {} findings to s3://{}/{}".format(len(results), S3_BUCKET, key))
+        response = s3.Bucket(S3_BUCKET).put_object(
+        Key=key,
+        Body=consolidated_results
+        )
+        logger.info("Successfully exported {} findings to s3://{}/{}".format(len(results), S3_BUCKET, key))
+    except ClientError as error_handle:
+            if error_handle.response['Error']['Code'] == 'ConnectTimeoutError':
+                time.sleep(5)
+                logger.warning('Catching Connection Timeout Error...')
+    except Exception as exception_handle:
+            logger.error(exception_handle)  
 
 def lambda_handler(event, context):
     print (event)
-    finding_filter = create_filter()
     if 'Payload' in event:
         next_token = event['Payload']['NextToken']
+        date_filter = event['Payload']['StartDate']
         logger.info("NextToken {} detected for Security Hub findings.".format(next_token))
     else:
         next_token = ''
+        date_filter=event['StartDate']
         logger.info("NextToken not detected for Security Hub findings.")
+    sechub = sechub_assume_role()
+    finding_filter = create_filter(date_filter)
     runs = 25
     for a in range(0, runs, 1):
         if (next_token is not None):
-            next_token, results, consolidated_results = get_findings(finding_filter, next_token)
+            next_token, results, consolidated_results = get_findings(sechub, finding_filter, next_token)
             put_obj_to_s3(results, consolidated_results)
             sechub_count = sechub_count_value(results)
         else:
@@ -159,5 +132,6 @@ def lambda_handler(event, context):
             break
     return {
         'NextToken': next_token,
-        'SecHubCount': sechub_count
+        'SecHubCount': sechub_count,
+        'StartDate': date_filter
     }
